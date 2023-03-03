5d739f4e01f5ab0bf209ae03f7eee119deea24f9
primarch93 <sugonegor@gmail.com>
2023-03-03 21:12:24 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -65,6 +65,19 @@ export class App extends Component {
         firstContact.name.localeCompare(secondContact.name)
       );
   };
+componentDidMount() {
+  const savedContacts = localStorage.getItem('contacts');
+  if (savedContacts) {
+    this.setState({ contacts: JSON.parse(savedContacts) });
+  }
+}
+
+  componentDidUpdate(prevProps, prevState) {
+  if (prevState.contacts !== this.state.contacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
+
 
   render() {
     return (
